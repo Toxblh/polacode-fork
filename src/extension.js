d1ba6d81a987db8599a63bbee1a17ca4a4d4b12d
Adrien Pyke <adpyke@gmail.com>
2019-09-23 15:33:53 -0400

    fix redundant if

--- src/extension.js
+++ src/extension.js
@@ -56,12 +56,10 @@ function activate(context) {
     
     const selectionHandler = vscode.window.onDidChangeTextEditorSelection(e => {
       if (e.selections[0] && !e.selections[0].isEmpty) {
-        if (e.selections[0] && !e.selections[0].isEmpty) {
-          vscode.commands.executeCommand('editor.action.clipboardCopyAction')
-          panel.webview.postMessage({
-            type: 'update'
-          })
-        }
+        vscode.commands.executeCommand('editor.action.clipboardCopyAction')
+        panel.webview.postMessage({
+          type: 'update'
+        })
       }
     })
 
